daad07fd73d54d385ca4c29faf4a4082a0ac1222
Sebastian Badon <s.badon@reply.com>
2025-03-25 09:36:31 -0400

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/admin/tools/OpenGL
+# Build directory: /Users/admin/tools/OpenGL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
