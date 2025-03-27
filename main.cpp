8e9c79c4f92c569d0f3665f0fd0acd2a31f70cda
Sebastian Badon <s.badon@reply.com>
2025-03-27 15:56:36 -0400

    Hellow window setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,91 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+void errorCallback(int error, const char* description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main(int, char**){
-    std::cout << "Hello, from OpenGL!\n";
+    std::cout << "Hello, world 2!" << std::endl;
+    
+    
+    // Initialise GLFW
+    glfwSetErrorCallback(errorCallback);
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    std::cout << "GLFW initialized" << std::endl;
+    std::cout.flush();
+    // Create a windowed mode window and its OpenGL context
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    std::cout << "Window hints set" << std::endl;
+    // Create a windowed mode window and its OpenGL context
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+    
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    std::cout.flush();
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    std::cout << "GLAD initialized" << std::endl;
+    std::cout.flush();
+    // Set the viewport
+    int width, height;
+    glViewport(0, 0, 800, 600);
+    
+    // Set the framebuffer size callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    std::cout << "Framebuffer size callback set" << std::endl;
+    std::cout.flush();
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        processInput(window);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+    // Cleanup
+    glfwTerminate();
+    std::cout << "GLFW terminated" << std::endl;
+    std::cout.flush();
+    return 0;
 }
